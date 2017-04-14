d6b8cf320360a455f4b539106209526d04292858
venky <venky504504@gmail.com>
2017-04-14 17:16:15 +0530

    added

--- ServerModules/serversample1/manifests/java.pp
+++ ServerModules/serversample1/manifests/java.pp
@@ -1,16 +1,12 @@
 # Class: 
 #
 #
-class serversample1::java(
-    $java_name = 'jdk6',
-    $java_version = '6',
-    $java_se = 'jdk'
-) {
+class serversample1::java{
 
-  java::oracle { $java_name :
+  java::oracle { 'jdk6' :
   ensure  => 'present',
-  version => $java_version,
-  java_se => $java_se,
+  version => '6',
+  java_se => 'jdk',
 }
     
 }    
